b2bf4c5eb59a3f17666dadb35c13f02983aef22a
Richiul <rpalkuc@yahoo.com>
2022-10-14 16:37:30 +0300

    2:02

--- vite-project/src/store/index.js
+++ vite-project/src/store/index.js
@@ -191,7 +191,7 @@ const store = createStore({
             token: sessionStorage.getItem("TOKEN"),
         },
         surveys: [...tmpSurveys],
-        
+        questionTypes: ["text","select","radio","checkbox","textarea"],
     },
     getters: {},
     actions: {
--- vite-project/src/views/SurveyView.vue
+++ vite-project/src/views/SurveyView.vue
@@ -190,6 +190,7 @@
 import { ref } from "vue";
 import { useRoute } from "vue-router";
 import store from "../store";
+import {v4 as uuidv4} from "uuid";
 
 import PageComponent from "../components/PageComponent.vue";
 import QuestionEditor from "../components/editor/QuestionEditor.vue";
@@ -211,6 +212,33 @@ if (route.params.id) {
         (s) => s.id === parseInt(route.params.id)
     );
 }
+
+function addQuestion(index) {
+    const newQuestion = {
+        id:uuidv4(),
+        type:"text",
+        question:"",
+        description:null,
+        data:{},
+    };
+    model.value.questions.splice(index,0,newQuestion);
+}
+
+function deleteQuestion(question) {
+    model.value.questions = model.value.questions.filter(
+        (q) => q !== question
+    );
+}
+
+function questionChange(question) {
+    model.value.questions = model.value.questions.map((q) => {
+        if(q.id === question.id) {
+            return JSON.parse(JSON.stringify(question));
+        }
+        return q;
+    });
+}
+
 </script>
 
 <style></style>
